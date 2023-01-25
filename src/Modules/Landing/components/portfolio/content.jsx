c0b3283a894482887fc4ff842cc6773251c57bed
Sam ugonna <sam_ug@yahoo.com>
2023-01-25 17:31:25 +0100

    Update content.jsx

--- src/Modules/Landing/components/portfolio/content.jsx
+++ src/Modules/Landing/components/portfolio/content.jsx
@@ -4,25 +4,23 @@ export const portfolioContent = {
       url: "https://umsep-annualreport.com/",
       alt: "University of Michigan School of Public Health - Nate Baker.me",
       showURL: "UMSEP Annual Report",
-      publicId: "personal/natebaker.me/umsep_img_ijqmh4",
+      publicId: "personal/sam-fullstack.netlify.app/umsep_img_ijqmh4",
     },
     {
       url: "https://www.kqhsllc.com/",
       alt: "Kaptin's Quality Home Solutions - Nate Baker.me",
       showURL: "Kaptain's Quality Home Solutions",
-      publicId: "personal/natebaker.me/kqhs_img_lazdfk",
+      publicId: "personal/sam-fullstack.netlify/kqhs_img_lazdfk",
     },
     {
       url: "/",
-      alt: "Nate Baker Personal Website - Nate Baker.me",
-      showURL: "Natebaker.me",
-      publicId: "personal/natebaker.me/natebaker_img_ltzn2d",
+  
     },
     {
-      url: "https://nate2427-makes-great-sites.netlify.app/",
-      alt: "Nate Baker Personal Site - Practice - Nate Baker.me",
+      url: "https://sam-fullstack.netlify.app/",
+      alt: "Sam Ugonna personal Site - Practice - Nate Baker.me",
       showURL: "Practice Site",
-      publicId: "personal/natebaker.me/practicesite_img_f9vtte",
+      publicId: "personal/sam-fullstack.netlify.app/practicesite_img_f9vtte",
     },
   ],
 };

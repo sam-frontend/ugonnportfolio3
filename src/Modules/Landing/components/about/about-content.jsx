7a9ad5523c8b856e66c3fe34ed5e92c148a15f0d
Sam ugonna <sam_ug@yahoo.com>
2023-10-01 05:53:15 +0100

    Update about-content.jsx

--- src/Modules/Landing/components/about/about-content.jsx
+++ src/Modules/Landing/components/about/about-content.jsx
@@ -2,9 +2,21 @@ export const aboutContent = {
   aboutBody:
     "I am Sam Ugonna i am a software engineer",
   skills: [
+     {
+      skill: "HTML",
+      status: 94,
+    },
+     {
+      skill: "CSS",
+      status: 70,
+    },
+     {
+      skill: "Tailwind",
+      status: 50,
+    },
     {
       skill: "React",
-      status: 94,
+      status: 90,
     },
      {
       skill: "React-native",
@@ -12,7 +24,7 @@ export const aboutContent = {
     },
     {
       skill: "Nodejs",
-      status: 25,
+      status: 40,
     },
     {
       skill: "problem solving,team work",
